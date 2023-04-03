a9a879d3571c36c62725d0a33a3f46c38fa54937
gracemcken <grace.ellen.mckenna@gmail.com>
2023-04-03 22:47:08 +0100

    Add account setup settings

--- main/settings.py
+++ main/settings.py
@@ -135,6 +135,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Account Setup
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = 'True'
+ACCOUNT_USERNAME_REQUIRED = 'True'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/account/login'
+LOGIN_REDIRECT_URL = '/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
