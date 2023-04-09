df5cc50989a7c8c3db8c29d3ae422e612ee4f81e
gracemcken <grace.ellen.mckenna@gmail.com>
2023-04-09 23:28:46 +0100

    Fix url_name bug in navbar for active class

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEVELOPMENT")
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
