ac002e7cc850e38d06d81df76a00983b064d2c15
gracemcken <grace.ellen.mckenna@gmail.com>
2023-04-10 02:14:11 +0100

    Add pattern type choices, remaining model info

--- patterns/models.py
+++ patterns/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from djrichtextfield.models import RichTextField
 from django_resized import ResizedImageField
 
+# Pattern Choice Fields
+
+PATTERN_TYPE = (
+    ('crossstitch', 'CrossStitch'),
+    ('blackwork', 'Blackwork'),
+    ('embroidery', 'Embroidery')
+)
+
 # Create your models here.
 
 
@@ -19,3 +27,14 @@ class Pattern(models.Model):
     image = ResizedImageField(
         size=[400, None], quality=75, upload_to='patterns/', force_format='WEBP', null=False, blank=False
     )
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    pattern_type = models.CharField(
+        max_length=50, choices=PATTERN_TYPE, default='mixed')
+    colors = models.IntegerField()
+    date_posted = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['date_posted']
+
+    def __str__(self):
+        return str(self.title)
