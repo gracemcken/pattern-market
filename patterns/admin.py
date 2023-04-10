54ee555372ab51dfd6977eb50a81c2b7cf8266c2
gracemcken <grace.ellen.mckenna@gmail.com>
2023-04-10 02:22:29 +0100

    Add Pattern to admin.py

--- patterns/admin.py
+++ patterns/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Pattern
 
 # Register your models here.
+
+
+@admin.register(Pattern)
+class PatternAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'pattern_type',
+        'colors',
+        'image',
+        'pattern_guide'
+    )
+
+    list_filter = ('pattern_type')
