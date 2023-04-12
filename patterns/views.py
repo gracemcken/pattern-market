965cf58889e643e72de81c6dc04bcc92cb026a44
gracemcken <grace.ellen.mckenna@gmail.com>
2023-04-13 00:36:06 +0100

    Create AddPattern view

--- patterns/views.py
+++ patterns/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Pattern
 
 # Create your views here.
+
+
+class AddPattern(CreateView):
+    """Add pattern view"""
+    template_name = 'patterns/add_pattern.html'
+    model = Pattern
+    success_url = '/patterns/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddPattern, self).form_valid(form)
