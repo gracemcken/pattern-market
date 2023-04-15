6e40d475e2e4d4aada127d1ae929013c1dac5c44
gracemcken <grace.ellen.mckenna@gmail.com>
2023-04-15 23:55:38 +0100

    Add url path for DeletePattern

--- patterns/urls.py
+++ patterns/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AddPattern, Patterns, PatternDetail
+from .views import AddPattern, Patterns, PatternDetail, DeletePattern
 
 # Url Patterns
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", AddPattern.as_view(), name="add_pattern"),
     path("patterns/", Patterns.as_view(), name="patterns"),
     path("<slug:pk>/", PatternDetail.as_view(), name="pattern_detail"),
+    path("delete/<slug:pk>/", DeletePattern.as_view(), name="delete_pattern"),
 ]
