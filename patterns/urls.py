9c502c36661f0cdb060a7fe6451ca2b792407329
gracemcken <grace.ellen.mckenna@gmail.com>
2023-04-16 00:00:04 +0100

    Fix url paths for add_pattern and patterns

--- patterns/urls.py
+++ patterns/urls.py
@@ -4,8 +4,8 @@ from .views import AddPattern, Patterns, PatternDetail, DeletePattern
 # Url Patterns
 
 urlpatterns = [
-    path("", AddPattern.as_view(), name="add_pattern"),
-    path("patterns/", Patterns.as_view(), name="patterns"),
+    path("add/", AddPattern.as_view(), name="add_pattern"),
+    path("", Patterns.as_view(), name="patterns"),
     path("<slug:pk>/", PatternDetail.as_view(), name="pattern_detail"),
     path("delete/<slug:pk>/", DeletePattern.as_view(), name="delete_pattern"),
 ]
