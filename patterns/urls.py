baac1d757e5233f8e45f2f391463d520b0767569
gracemcken <grace.ellen.mckenna@gmail.com>
2023-04-15 14:15:53 +0100

    Add PatternDetail path to urls.py

--- patterns/urls.py
+++ patterns/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddPattern, Patterns
+from .views import AddPattern, Patterns, PatternDetail
 
 # Url Patterns
 
 urlpatterns = [
     path("", AddPattern.as_view(), name="add_pattern"),
     path("patterns/", Patterns.as_view(), name="patterns"),
+    path("<slug:pk>/", PatternDetail.as_view(), name="pattern_detail"),
 ]
